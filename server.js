1047be65cdbc4b39dc15e909ab6f5079c0d0720a
Joshua Stanford <stanjosh@gmail.com>
2023-07-22 20:46:40 -0500

    hbs and api working together nicely w ex router

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
+const path = require('path');
 const express = require('express');
 const routes = require('./routes');
-const { Sequelize } = require('sequelize');
-// import sequelize connection
-
+const views = require('./views');
+const engine = require('express-handlebars');
 const sequelize = require('./config/connection');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+var hbs = engine.create(
+    {
+      extname: '.hbs',
+      defaultLayout: 'main',
+      layoutsDir: `${__dirname}/views/layouts`,
+      partialsDir: `${__dirname}/views/partials`,
+});
 
+app.engine('hbs',  hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', `${__dirname}/views`);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(views);
 app.use(routes);
+
+
 
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false })
